28e28951abd15576ea4644e78d8859e64f89b81c
andinasalamah <andina.salamah@gmail.com>
2019-04-19 15:18:38 +0700

    0.2.6 -- Sprint2 : Added List

---
+++ app/src/main/java/com/example/android/splendidbandung/ListViewWorship.java
@@ -0,0 +1,23 @@
+package com.example.android.splendidbandung;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+public class ListViewWorship extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListViewWorship ListViewWorship;
+    ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_worship);
+
+        ListViewWorship = (ListViewWorship findViewById(R.id.ListViewWorship);
+        adapter = ArrayAdapter.createFromResource(this,R.array.Worship,android.R.layout.simple_list_item_1);
+        ListViewWorship.setAdapter(adapter);
+        ListViewWorship.setOnItemClickListener(this);
+    }
+
+}
