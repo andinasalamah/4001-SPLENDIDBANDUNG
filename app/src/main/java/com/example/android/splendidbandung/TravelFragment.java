09693e89e0f5ef497d8e99a1ba14aef33f8e6a34
leonardotaufans <leonardotaufans@gmail.com>
2019-04-26 20:11:14 +0700

    0.3.2 -- Nyan

--- app/src/main/java/com/example/android/splendidbandung/TravelFragment.java
+++ app/src/main/java/com/example/android/splendidbandung/TravelFragment.java
@@ -10,72 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
-public class TravelFragment extends Fragment implements View.OnClickListener{
+public class TravelFragment extends Fragment {
     ImageButton bandara, belanja, penginapan, rekreasi, rmhmakan, ibadah, travel, wisata;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_travel, container, false);
-        init(view);
+        View view = inflater.inflate(R.layout.fragment_travel, container, false);
         return view;
     }
-
-    private void init(View view) {
-        bandara = view.findViewById(R.id.bandara);
-        penginapan = view.findViewById(R.id.penginapan);
-        belanja = view.findViewById(R.id.belanja);
-        rekreasi = view.findViewById(R.id.rekreasi);
-        rmhmakan = view.findViewById(R.id.rmhmakan);
-        ibadah = view.findViewById(R.id.ibadah);
-        travel = view.findViewById(R.id.travel);
-        wisata = view.findViewById(R.id.wisata);
-
-        bandara.setOnClickListener(this);
-        penginapan.setOnClickListener(this);
-        belanja.setOnClickListener(this);
-        rekreasi.setOnClickListener(this);
-        rmhmakan.setOnClickListener(this);
-        ibadah.setOnClickListener(this);
-        travel.setOnClickListener(this);
-        wisata.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(getActivity(), MapsActivity.class);
-        switch (v.getId()) {
-            case R.id.bandara:
-                intent.putExtra("tujuan", "bandara");
-                startActivity(intent);
-                break;
-            case R.id.belanja:
-                intent.putExtra("tujuan", "belanja");
-                startActivity(intent);
-                break;
-            case R.id.penginapan:
-                intent.putExtra("tujuan", "penginapan");
-                startActivity(intent);
-                break;
-            case R.id.rekreasi:
-                intent.putExtra("tujuan", "rekreasi");
-                startActivity(intent);
-                break;
-            case R.id.rmhmakan:
-                intent.putExtra("tujuan", "rumahmakan");
-                startActivity(intent);
-                break;
-            case R.id.ibadah:
-                intent.putExtra("tujuan", "ibadah");
-                startActivity(intent);
-                break;
-            case R.id.travel:
-                intent.putExtra("tujuan", "travel");
-                startActivity(intent);
-                break;
-            case R.id.wisata:
-                intent.putExtra("tujuan", "wisata");
-                startActivity(intent);
-                break;
-        }
-    }
 }
+
